355621162c04206cac86121604394d2aadb07103
serenity wellness <iniserenityresort@gmail.com>
2025-08-07 07:42:30 +0000

    https://images.seeklogo.com/logo-png/40/1/shopee-pay-logo-png_seeklogo-4

--- next.config.ts
+++ next.config.ts
@@ -21,6 +21,12 @@ const nextConfig: NextConfig = {
         hostname: 'upload.wikimedia.org',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.seeklogo.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
--- src/app/(app)/pricing/page.tsx
+++ src/app/(app)/pricing/page.tsx
@@ -58,7 +58,7 @@ const tiers = [
 const ewallets = [
     { name: 'GoPay', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/8/86/Gopay_logo.svg/308px-Gopay_logo.svg.png?20210531070158', alt: 'GoPay Logo', hint: 'gopay logo' },
     { name: 'OVO', logo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/e/eb/Logo_ovo_purple.svg/1920px-Logo_ovo_purple.svg.png', alt: 'OVO Logo', hint: 'ovo logo' },
-    { name: 'ShopeePay', logo: '/images/shopeepay.png', alt: 'ShopeePay Logo', hint: 'shopeepay logo' },
+    { name: 'ShopeePay', logo: 'https://images.seeklogo.com/logo-png/40/1/shopee-pay-logo-png_seeklogo-406839.png', alt: 'ShopeePay Logo', hint: 'shopeepay logo' },
     { name: 'DANA', logo: '/images/dana.png', alt: 'DANA Logo', hint: 'dana logo' },
 ];
 
